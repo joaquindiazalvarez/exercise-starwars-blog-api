f28ac0423b66b34f1e1e866ea38d1b2ca5d5347a
joaquindiazalvarez <joaquindiazalvarez@gmail.com>
2022-04-22 01:35:24 +0000

    actualizado de la clase

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets, Fav_people
 #from models import Person
 
 app = Flask(__name__)
@@ -31,15 +31,67 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_user():
+    all_users = User.query.all()
+    array_users = list(map(lambda x: x.serialize(), all_users))
+    return jsonify({"users":array_users})
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
-
+@app.route('/people', methods=['GET'])
+def get_people():
+    all_people = People.query.all()
+    array_people = list(map(lambda x : x.serialize(), all_people))
+    return jsonify({"mensaje":array_people})
 # this only runs if `$ python src/main.py` is executed
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_people_id():
+    one_people = People.query.get(people_id)
+    if one_people:
+        return jsonify({"personaje": one_people.serialize()})
+    else:
+        return "te equivocaste: :("
+@app.route('/planets', methods = ['GET'])
+def get_planets():
+    all_planets = Planets.query.all()
+    array_planets = list(map(lambda x : x.serialize(), all_planets))
+    return jsonify({"mensaje": array_planets})
+
+@app.route('/planets/<int:planets_id>', methods = ['GET'])
+def get_planets_id():
+    one_planet = Planets.query.get(planets_id)
+    if one_planet:
+        return jsonify({"personaje": one_people.serialize()})
+    else:
+        return "te equivocaste: :("
+
+@app.route('/fav_people', methods=['GET'])
+def get_fav_people():
+    all_fav_people = Fav_people.query.all()
+    array_fav_people = list(map(lambda x : x.serialize(), all_fav_people))
+    return jsonify({"mensaje":array_fav_people})
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def add_fav_people(people_id):
+    user = request.get_json()
+    checkuser = User.query.get(user['id'])
+    if checkuser:
+        new_fav = Fav_people()
+        new_fav.id_user = user['id']
+        new_fav.uid_people = people_id
+        db.session.add(new_fav)
+        db.session.commit()
+        return("todo salio bien :D")
+    else:
+        return ("el usuario no existe")
+    
+@app_route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def deleteFavPeople(people_id):
+    user = request.get_json()
+    all_fav = Fav_people.query.filter_by(id_user=user['id'], uid_people = people_id).all()
+    for i in allFavs:
+        db.session.delete(i)
+    db.session.commit()
+    return ("ok")
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
